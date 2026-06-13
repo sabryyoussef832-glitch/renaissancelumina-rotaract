4e4d7016b73e79d507c218c427c2a9a66d7a2b09
sabryyoussef832-glitch <sabryyoussef832@gmail.com>
2026-06-13 23:43:52 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -348,4 +348,5 @@ def admin_dashboard():
     })
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=True)
